bceea06073aa024819faa0de212f74702a866f7f
NashifAlam <nashifalamscar@gmail.com>
2021-04-12 21:57:14 +0530

    Added react dependencies

---
+++ src/index/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import HomeIMG from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <HomeIMG/>
+
+       
+        
+          ENCOM
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
