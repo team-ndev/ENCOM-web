eb2e0439e15e7c9cf446519a8bdfe15506286560
Nashif Alam <nashifalamscar@gmail.com>
2021-04-15 11:27:54 +0530

    Added tabbed activities  (#6)
    * Add Hello World  (#2)
    
    * added license
    
    * Added react dependencies
    
    * Update README.md
    
    * Relocation
    
    * Added code of conduct
    
    * Update README.md
    
    * Hello World (#1)
    
    * Added hello world
    
    * changes made (#5)
    
    * Implement Header, added dependencies & component (#4)
    
    - Implemented header with react router
    - added material ui and react-router-dom
    - added components Notification, Live & Channels
    
    Co-authored-by: Nashif Alam <nashifalamscar@gmail.com>
    
    Co-authored-by: shivaniii24 <64369944+shivaniii24@users.noreply.github.com>
    Co-authored-by: Deepesh Rathod <60640528+deepesh-01@users.noreply.github.com>

---
+++ src/index/src/components/Live/live.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import useStyles from './liveStyles';
+
+export const Live = () =>{
+    const classes = useStyles();
+    return(
+        <div><h1>Live Router is working!</h1></div>
+    );
+}
