237aebcf5f8d5b32bc817b968187e3d74488a851
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2021-04-18 09:51:00 +0530

    changed styling

--- src/index/src/components/custom-button/custom-button.component.jsx
+++ src/index/src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './custom-buttom.styles.scss';
+import './custom-buttom.styles.css';
 
 const CustomButton = ({ children, ...otherProps }) => (
   <button className='custom-button' {...otherProps}>
--- src/index/src/components/form-input/form-input.component.jsx
+++ src/index/src/components/form-input/form-input.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './form-input.styles.scss';
+import './form-input.styles.css';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
