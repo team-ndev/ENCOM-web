9015d3f20a2c062cadf52ac90045e986db05a431
NashifAlam <nashifalamscar@gmail.com>
2021-05-03 19:44:22 +0530

    Minor changes in text

--- src/index/src/components/Channels/channels.js
+++ src/index/src/components/Channels/channels.js
@@ -4,6 +4,6 @@ import useStyles from './channelsStyles';
 export const Channels = () => {
     const classes = useStyles();
     return(
-        <div><h2>LOGS   @ChannelName1   @ChannelName2</h2></div>
+        <div><h2>LOGS  @ChannelName1   @ChannelName2   @ChannelName3</h2></div>
     );
 }
